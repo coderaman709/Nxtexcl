6832b2b746daa41754af23df36f6f6d7f7793a6c
coderaman709 <114331672+coderaman709@users.noreply.github.com>
2024-03-25 11:04:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function animation(){
+    const an1 = document.getElementById("an1");
+    const an2 = document.getElementById("an2");
+    const an3 = document.getElementById("an3");
+    const an4 = document.getElementById("an4");
+
+    an1.style.margin = "0 30px";
+    an1.style.color = "rgb(20, 109, 105)";
+
+    an2.style.margin = "10 130px";
+    an2.style.color = "rgb(53, 115, 112)";
+
+    an3.style.margin = "20 10px";
+    an3.style.color = "rgb(50, 146, 141)";
+
+    an4.style.margin = "40 160px";
+    an4.style.backgroundColor = "rgb(42, 129, 125)";
+}
